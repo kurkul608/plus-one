77ff48d793b49d3f8bfb6b305f328927f34c995e
Petr Molchanov <molc.pyotr@yandex.ru>
2021-01-23 23:28:23 +0400

    fix history page

--- assets/js/mainPage/index.js
+++ assets/js/mainPage/index.js
@@ -9,34 +9,6 @@ window.addEventListener("scroll", () =>
     : null
 );
 
-const slider = document.querySelector(".sliderBlock__slider");
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener("mousedown", (e) => {
-  // slider.classList.add("active");
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-  // console.log(startX);
-  isDown = true;
-});
-slider.addEventListener("mouseleave", () => {
-  isDown = false;
-  slider.classList.remove("active");
-});
-slider.addEventListener("mouseup", () => {
-  isDown = false;
-});
-slider.addEventListener("mousemove", (e) => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3;
-  // console.log(walk);
-  slider.scrollLeft = scrollLeft - walk;
-});
-
 function scrollDown() {
   var windowCoords = document.documentElement.clientHeight;
   (function scroll() {
@@ -135,8 +107,6 @@ if (animItems.length > 0) {
     }
   }
 
-
-  
   function offset(el) {
     const rect = el.getBoundingClientRect(),
       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
